33c94dfa65e4616e447daa8b685281feb31e26f5
twsomt <volk0v-ak@yandex.ru>
2023-01-03 23:45:54 +0300

    Add registration

--- randomizer/users/urls.py
+++ randomizer/users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.views import LogoutView 
 from django.urls import path
+from users.views import *
 
 app_name = 'users'
 
 urlpatterns = [
-    path(
-      'logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+    path('signup/', SignUp.as_view(), name='signup')
 ]
