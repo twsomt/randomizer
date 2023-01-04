2074291ddd5fa115504164916ab0a965e205f484
twsomt <volk0v-ak@yandex.ru>
2023-01-04 22:22:31 +0300

    Add login, html template from update password, restore password

--- randomizer/randomizer/settings.py
+++ randomizer/randomizer/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'generator',
     'users.apps.UsersConfig',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+
+LOGIN_REDIRECT_URL = 'generator:index'
--- randomizer/users/urls.py
+++ randomizer/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView 
+from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
 from django.urls import path
 from users.views import *
 
@@ -6,5 +6,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
-    path('signup/', SignUp.as_view(), name='signup')
+    path('signup/', SignUp.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='users/login.html'),name='login'
+    ),
 ]
