230ea57910611a84246a2ecf0e4e9be56de0c112
twsomt <volk0v-ak@yandex.ru>
2023-01-06 01:57:00 +0300

    transfer

--- randomizer/users/urls.py
+++ randomizer/users/urls.py
@@ -1,4 +1,12 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import (LogoutView,
+                                       LoginView,
+                                       PasswordChangeView,
+                                       PasswordChangeDoneView,
+                                       PasswordResetView,
+                                       PasswordResetDoneView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetCompleteView
+                                    )
 from django.urls import path
 from users.views import *
 
@@ -8,8 +16,13 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
     path('signup/', SignUp.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='users/login.html'),name='login'),
-
-
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     path('password_change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+
+
+
+    path('password_reset/', PasswordResetView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_change_form.html'), name='password_change'),
 ]
