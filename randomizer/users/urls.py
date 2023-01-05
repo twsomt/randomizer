2b521df9cec98baf9d99e5a4130c7e1b3b834c87
twsomt <volk0v-ak@yandex.ru>
2023-01-06 01:05:22 +0300

    Add change_password page

--- randomizer/users/urls.py
+++ randomizer/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
+from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 from users.views import *
 
@@ -7,6 +7,9 @@ app_name = 'users'
 urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
     path('signup/', SignUp.as_view(), name='signup'),
-    path('login/', LoginView.as_view(template_name='users/login.html'),name='login'
-    ),
+    path('login/', LoginView.as_view(template_name='users/login.html'),name='login'),
+
+
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+    path('password_change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
 ]
