d61890cb1670c4f2b559871301b330b0f660e472
twsomt <volk0v-ak@yandex.ru>
2023-01-07 01:54:49 +0300

    Add my_raffles page

--- randomizer/generator/urls.py
+++ randomizer/generator/urls.py
@@ -4,7 +4,8 @@ from generator.views import *
 app_name = 'generator'
 urlpatterns = [
     path('', index, name='index'),
-    path('raffles/<slug:slug>/', raffle_page, name='raffle_page')
+    path('raffles/<slug:slug>/', raffle_page, name='raffle_page'),
+    path('my_raffles/', my_raffles, name='my_raffles'),
 ]
 
 
--- randomizer/generator/views.py
+++ randomizer/generator/views.py
@@ -22,10 +22,20 @@ def index(request):
 
 @authorized_only
 def raffle_page(request, slug):
-    raffles = (Raffle.objects.filter(slug=slug))
+    raffles = (Raffle.objects.filter(creator=request.user, slug=slug))
 
     context = {
         'raffles': raffles
     }
 
     return render(request, 'generator/raffle_page.html', context)
+
+@authorized_only
+def my_raffles(request):
+    raffles = (Raffle.objects.filter(creator=request.user))
+
+    context = {
+        'raffles': raffles
+    }
+
+    return render(request, 'generator/my_raffles.html', context)
