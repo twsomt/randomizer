fbeaa697336a815dd899903afb79c44f7016e050
twsomt <volk0v-ak@yandex.ru>
2023-01-08 21:27:57 +0300

    Add verbouse_name in models

--- randomizer/generator/models.py
+++ randomizer/generator/models.py
@@ -9,18 +9,20 @@ User = get_user_model()
 class Raffle(models.Model):
 
     # Fields
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, null=False)
-    description = models.TextField(max_length=500)
-    link = models.URLField()
-    is_subscribers = models.BooleanField(default=True)
-    winner = models.TextField()
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name='')
+    slug = models.SlugField(unique=True, null=False, verbose_name='')
+    description = models.TextField(max_length=500, verbose_name='')
+    link = models.URLField(verbose_name='')
+    is_subscribers = models.BooleanField(default=True, verbose_name='')
+    qty_winners = models.PositiveIntegerField(verbose_name='')
+    winner = models.TextField(verbose_name='')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='')
     creator = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='raffles',
+        verbose_name=''
     )
 
     class Meta:
@@ -33,9 +35,9 @@ class Raffle(models.Model):
 class Client(models.Model):
 
     # Fields
-    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
-    api_vk_key = models.CharField(null=True, max_length=500)
-    paid_up_to = models.DateField(null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, verbose_name='')
+    api_vk_key = models.CharField(null=True, max_length=500, verbose_name='')
+    paid_up_to = models.DateField(null=True, verbose_name='')
 
     def __str__(self):
         return self.user.get_username()
