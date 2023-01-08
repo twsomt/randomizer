09122fee1a7ecdf23991b60e2bd4361a975c34b2
twsomt <volk0v-ak@yandex.ru>
2023-01-08 03:35:29 +0300

    Add raffle form

--- randomizer/generator/models.py
+++ randomizer/generator/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
+# from django.core.validators import MinValueValidator
 
 User = get_user_model()
 
