8019c5e8140b793c1980d6abfb6d23b49aab9856
twsomt <volk0v-ak@yandex.ru>
2023-01-13 22:19:08 +0300

    Add secure

--- randomizer/users/urls.py
+++ randomizer/users/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import (LogoutView,
                                        PasswordResetView,
                                        PasswordResetDoneView,
                                        PasswordResetConfirmView,
-                                       PasswordResetCompleteView
+                                       PasswordResetCompleteView,
                                     )
 from django.urls import path
 from users.views import *
@@ -22,4 +22,5 @@ urlpatterns = [
     path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('access_denied/', access_denied, name='access_denied'),
 ]
--- randomizer/users/views.py
+++ randomizer/users/views.py
@@ -1,10 +1,22 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CreationForm
+from django.shortcuts import render, redirect
 
+def secret_enter(func):
+    def check_user(request, *args, **kwargs):
+        password = self.request.GET.get('pswrd', '')
+        if password == 'lkjr6tyyr416vvv35445dtyb':
+            return func(request, *args, **kwargs)
+        return redirect('access_denied/')
+    return check_user
 
+# @secret_enter
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('generator:index')
     template_name = 'users/signup.html'
     
+
+def access_denied(request):
+    return render(request, 'users/access_denied.html')
