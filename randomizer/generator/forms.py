ad704b4189134de0801ca76b78c1d7b0552e6b26
twsomt <volk0v-ak@yandex.ru>
2023-01-24 22:15:01 +0300

    Fix forms

--- randomizer/generator/forms.py
+++ randomizer/generator/forms.py
@@ -1,25 +1,11 @@
-class Bird:
-    def __init__(self, name, size):
-        self.name = name
-        self.size = size
+from django import forms
 
-    def describe(self):
-        return f'Размер птицы {self.name} — {self.size}.'
+from generator.models import Raffle
 
 
-class Parrot(Bird):
-    def __init__(self, name, size, color):
-        super().__init__(name)
-        super().__init__(size)
-        self.color = color
-
-
-class Penguin(Bird):
-    def __init__(self, name, size, genus):
-        super().__init__(name)
-        super().__init__(size)
-        self.genus = genus
-    
-    
-kesha = Parrot('Ара', 'средний', 'красный')
-kowalski = Penguin('Королевский', 'большой', 'Aptenodytes')
+class VkForm(forms.ModelForm):
+    class Meta:
+        # Raffle form
+        model = Raffle
+        fields = ('description', 'link',
+                  'qty_winners', 'is_subscribers')
